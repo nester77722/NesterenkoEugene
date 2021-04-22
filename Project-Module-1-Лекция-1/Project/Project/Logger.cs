4d4caab3f22718c61a07b4c5bb15d9eb1d8e6b8a
Eugene <nester77722@gmail.com>
2021-04-22 20:05:26 +0300

    Реализован Синглтон в Logger

---
+++ Project-Module-1-Лекция-1/Project/Project/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Logger
+    {
+        private static Logger _uniqueInstance;
+
+        private StringBuilder _output;
+
+        private DateTime _dateTime;
+
+        private Logger()
+        {
+            _output = new StringBuilder();
+        }
+
+        public static Logger Instance()
+        {
+            if (_uniqueInstance == null)
+            {
+                _uniqueInstance = new Logger();
+            }
+
+            return _uniqueInstance;
+        }
+    }
+}
