f5bdcb0ffaade9595e62efcb1a6463353dc7f136
Eugene <nester77722@gmail.com>
2021-04-22 21:32:05 +0300

    Добавил класс Starter

---
+++ Project-Module-1-Лекция-1/Project/Project/Starter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Starter
+    {
+        private Actions _actions;
+
+        private Logger _logger;
+
+        public Starter()
+        {
+            _actions = new Actions();
+
+            _logger = Logger.Instance;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < 10000000; i++)
+            {
+                int random = new Random().Next(1, 4);
+
+                switch (random)
+                {
+                    case 1:
+                        {
+                            _actions.InfoResult();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            _actions.WarningResult();
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            _logger.AddLog(_actions.ErrorResult());
+                            break;
+                        }
+                }
+            }
+
+            Console.WriteLine(_logger.GetLogs());
+        }
+    }
+}
