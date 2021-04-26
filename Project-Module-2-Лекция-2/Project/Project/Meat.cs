468820082cb1924388af7d49027ad86309c2ece1
Eugene <nester77722@gmail.com>
2021-04-26 21:22:31 +0300

    Реализовал класс Meat и добавил enum Animals

---
+++ Project-Module-2-Лекция-2/Project/Project/Meat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Meat : Product
+    {
+        public Meat(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount, Animals animals)
+            : base(name, energy, proteinsCount, fatsCount, carbohydratesCount, false)
+        {
+            Animals = animals;
+        }
+
+        public Meat(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount, bool isFrozen, Animals animals)
+            : this(name, energy, proteinsCount, fatsCount, carbohydratesCount, animals)
+        {
+            IsFrozen = isFrozen;
+        }
+
+        public bool IsFrozen { get; set; }
+
+        public Animals Animals { get; private set; }
+    }
+}
