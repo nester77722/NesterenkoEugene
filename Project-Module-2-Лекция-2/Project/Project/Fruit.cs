74d3f61bf4f17af731e3b97c876143222a0da9b7
Eugene <nester77722@gmail.com>
2021-04-26 21:04:05 +0300

    Добавил классы Fruit и Vegetable

---
+++ Project-Module-2-Лекция-2/Project/Project/Fruit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Fruit : Product
+    {
+        public Fruit(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount, double millilitersOfJuicePerKilogram)
+    : base(name, energy, proteinsCount, fatsCount, carbohydratesCount, true)
+        {
+            MillilitersOfJuicePerKilogram = millilitersOfJuicePerKilogram;
+        }
+
+        public double MillilitersOfJuicePerKilogram { get; private set; }
+    }
+}
---
+++ Project-Module-2-Лекция-2/Project/Project/Vegetable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Vegetable : Product
+    {
+        public Vegetable(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount)
+            : base(name, energy, proteinsCount, fatsCount, carbohydratesCount, false)
+        {
+        }
+
+        public Vegetable(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount, double millilitersOfJuicePerKilogram)
+            : base(name, energy, proteinsCount, fatsCount, carbohydratesCount, true)
+        {
+            MillilitersOfJuicePerKilogram = millilitersOfJuicePerKilogram;
+        }
+
+        public double MillilitersOfJuicePerKilogram { get; private set; }
+    }
+}
