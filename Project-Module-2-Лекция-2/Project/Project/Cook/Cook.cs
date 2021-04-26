effd1e02efcb1532afa36efecc37337492bdadee
Eugene <nester77722@gmail.com>
2021-04-26 21:40:14 +0300

    Создал Cook, Human, ICook

---
+++ Project-Module-2-Лекция-2/Project/Project/Cook/Cook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Cook
+{
+    public class Cook : Human, ICook
+    {
+        public Cook(string name)
+            : base(name)
+        {
+        }
+
+        public Dish.Dish CookDish(string name, Product[] products)
+        {
+            return new Dish.Dish(name, products);
+        }
+
+        public void DecorateDish()
+        {
+            Console.WriteLine($"Cook {Name} decorated dish.");
+        }
+    }
+}
