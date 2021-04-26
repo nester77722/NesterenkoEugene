6a07e574ed2a4a75b4cc08a7c5d292ba8413d0d8
Eugene <nester77722@gmail.com>
2021-04-26 20:55:19 +0300

    Реализован класс Product

---
+++ Project-Module-2-Лекция-2/Project/Project/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Product
+    {
+        public Product(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount)
+        {
+            Name = name;
+
+            Energy = energy;
+
+            ProteinsCount = proteinsCount;
+
+            FatsCount = fatsCount;
+
+            CarbohydratesCount = carbohydratesCount;
+
+            IsJuiceable = false;
+        }
+
+        public Product(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount, bool isJuiceable)
+        {
+            Name = name;
+
+            Energy = energy;
+
+            ProteinsCount = proteinsCount;
+
+            FatsCount = fatsCount;
+
+            CarbohydratesCount = carbohydratesCount;
+
+            IsJuiceable = isJuiceable;
+        }
+
+        public string Name { get; private set; }
+
+        public double Energy { get; private set; }
+
+        public double ProteinsCount { get; private set; }
+
+        public double FatsCount { get; private set; }
+
+        public double CarbohydratesCount { get; private set; }
+
+        // Можно ли делать сок из продукта
+        public bool IsJuiceable { get; private set; }
+    }
+}
