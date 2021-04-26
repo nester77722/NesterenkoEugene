80396521c21ce237675d8145eca9f85b2c7824d0
Eugene <nester77722@gmail.com>
2021-04-26 21:13:00 +0300

    Реализовал класс Spice
    Класс специй

---
+++ Project-Module-2-Лекция-2/Project/Project/Spice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Spice : Product
+    {
+        public Spice(string name, double energy, double proteinsCount, double fatsCount, double carbohydratesCount, bool isSalty, bool isSpicy)
+            : base(name, energy, proteinsCount, fatsCount, carbohydratesCount, false)
+        {
+            IsSalty = isSalty;
+
+            IsSpicy = isSpicy;
+        }
+
+        public bool IsSalty { get; private set; }
+
+        public bool IsSpicy { get; private set; }
+    }
+}
