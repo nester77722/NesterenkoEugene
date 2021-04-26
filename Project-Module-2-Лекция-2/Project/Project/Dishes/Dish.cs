bf47f8cc80bd4240616286f26956cfc0e6225b15
Eugene <nester77722@gmail.com>
2021-04-26 21:33:15 +0300

    Реализовал класс Dish

---
+++ Project-Module-2-Лекция-2/Project/Project/Dishes/Dish.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Dishes
+{
+    public class Dish
+    {
+        private Product[] _products;
+
+        public Dish(string name, Product[] products)
+        {
+            Name = name;
+
+            _products = products;
+        }
+
+        public string Name { get; private set; }
+
+        public double Energy()
+        {
+            double count = 0;
+
+            foreach (var product in _products)
+            {
+                count += product.Energy;
+            }
+
+            return count;
+        }
+    }
+}
