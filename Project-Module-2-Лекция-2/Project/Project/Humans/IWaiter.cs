f205e870ced45fa87aababeabaa24ee5e80f3845
Eugene <nester77722@gmail.com>
2021-04-28 15:20:24 +0300

    Добавил интерфейс IWaiter и класс Waiter

---
+++ Project-Module-2-Лекция-2/Project/Project/Humans/IWaiter.cs
@@ -0,0 +1,12 @@
+﻿using Project.Dishes;
+
+namespace Project.Humans
+{
+    public interface IWaiter
+    {
+        public Product[] GetProductsFromDishSortedByEnergy(Dish dish);
+        public Product[] GetProductsFromDishSortedByCarbohydrates(Dish dish);
+        public Product[] GetProductsFromDishSortedByProteins(Dish dish);
+        public Product[] GetProductsFromDishSortedByFats(Dish dish);
+    }
+}
---
+++ Project-Module-2-Лекция-2/Project/Project/Humans/Waiter.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Project.Dishes;
+
+namespace Project.Humans
+{
+    public class Waiter : IWaiter
+    {
+        public Product[] GetProductsFromDishSortedByCarbohydrates(Dish dish)
+        {
+            var products = dish.GetProducts();
+
+            Product[] sortedProducts = products.OrderBy(o => o.CarbohydratesCount).ToArray();
+
+            return sortedProducts;
+        }
+
+        public Product[] GetProductsFromDishSortedByEnergy(Dish dish)
+        {
+            var products = dish.GetProducts();
+
+            Product[] sortedProducts = products.OrderBy(o => o.Energy).ToArray();
+
+            return sortedProducts;
+        }
+
+        public Product[] GetProductsFromDishSortedByFats(Dish dish)
+        {
+            var products = dish.GetProducts();
+
+            Product[] sortedProducts = products.OrderBy(o => o.FatsCount).ToArray();
+
+            return sortedProducts;
+        }
+
+        public Product[] GetProductsFromDishSortedByProteins(Dish dish)
+        {
+            var products = dish.GetProducts();
+
+            Product[] sortedProducts = products.OrderBy(o => o.ProteinsCount).ToArray();
+
+            return sortedProducts;
+        }
+    }
+}
