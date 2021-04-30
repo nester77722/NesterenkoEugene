aaa42ddf41c061b96bea3ce6d2b29eb9af8f8504
Eugene <nester77722@gmail.com>
2021-04-30 16:56:48 +0300

    Создал методы добавления элементов

--- Project-Module-2-Задание-1/Project/MyCollections/MyLinkedList.cs
+++ Project-Module-2-Задание-1/Project/MyCollections/MyLinkedList.cs
@@ -8,5 +8,36 @@ namespace MyCollections
         private MyLinkedListNode _tail;
 
         public int Count { get; private set; }
+        public void AddFirst(object value)
+        {
+            MyLinkedListNode node = new MyLinkedListNode(value);
+            node.Next = _head;
+            _head = node;
+
+            if (Count == 0)
+            {
+                _tail = _head;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(object value)
+        {
+            MyLinkedListNode node = new MyLinkedListNode(value);
+
+            if (_head == null)
+            {
+                _head = node;
+                _tail = node;
+            }
+            else
+            {
+                _tail.Next = node;
+                _tail = node;
+            }
+
+            Count++;
+        }
     }
 }
