fcdc2e4a54098660fe489b89e37502e757988bce
Eugene <nester77722@gmail.com>
2021-04-30 16:54:56 +0300

    Создал класс MyLinkedList

---
+++ Project-Module-2-Задание-1/Project/MyCollections/MyLinkedList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCollections
+{
+    public class MyLinkedList : IMyLinkedList
+    {
+        private MyLinkedListNode _head;
+        private MyLinkedListNode _tail;
+
+        public int Count { get; private set; }
+    }
+}
