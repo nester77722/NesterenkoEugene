ca2662eba61ec6d98ed412dd6bfb91ab5c10a838
Eugene <nester77722@gmail.com>
2021-05-10 20:20:07 +0300

    Добавил MyCollections

---
+++ Project-Module-2-Задание-2/MyProject/MyCollections/MyLinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿namespace MyCollections
+{
+    internal class MyLinkedListNode
+    {
+        internal MyLinkedListNode(object value)
+        {
+            Value = value;
+        }
+
+        internal MyLinkedListNode Next { get; set; }
+
+        internal object Value { get; set; }
+
+        internal void Invalidate()
+        {
+            Next = null;
+            Value = null;
+        }
+    }
+}
