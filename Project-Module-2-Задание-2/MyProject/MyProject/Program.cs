10d3961b70ae74cd63375ec1da3c3de494739b0a
Eugene <nester77722@gmail.com>
2021-05-12 12:27:23 +0300

    Изменения в Program

--- Project-Module-2-Задание-2/MyProject/MyProject/Program.cs
+++ Project-Module-2-Задание-2/MyProject/MyProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyCollections;
 
 namespace MyProject
 {
@@ -6,6 +7,51 @@ namespace MyProject
     {
         public static void Main(string[] args)
         {
+            MyDoublyLinkedList list = new MyDoublyLinkedList();
+
+            list.AddFirst("Hello");
+
+            list.AddLast("world");
+
+            list.AddLast("!");
+            list.AddLast("!");
+            list.AddLast("!");
+            list.AddLast("!");
+
+            list.RemoveByIndex(0);
+
+            list.AddFirst("Hi");
+
+            // check = true
+            var check = list.RemoveEveryEqual("!");
+
+            var array = list.ToArray();
+
+            // Hi
+            // world
+            foreach (var @object in array)
+            {
+                Console.WriteLine(@object);
+            }
+
+            list.AddFirst("Bob");
+            list.AddFirst("Eugene");
+            list[0] = "Zero";
+
+            list.RemoveByIndex(1);
+            list.RemoveFirst();
+
+            array = list.ToArray();
+
+            // Bob
+            // Hi
+            // world
+            foreach (var @object in array)
+            {
+                Console.WriteLine(@object);
+            }
+
+            list.Clear();
         }
     }
 }
