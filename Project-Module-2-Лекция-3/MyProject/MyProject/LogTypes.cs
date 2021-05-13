d5f1a2fac1a1cb4a2aa41b673e6241b1f8708f0e
Eugene <nester77722@gmail.com>
2021-05-13 20:19:42 +0300

    Сделал Enum LogTypes

---
+++ Project-Module-2-Лекция-3/MyProject/MyProject/LogTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public enum LogTypes
+    {
+        Info,
+        Warning,
+        Error
+    }
+}
