950bdbdc8bfa98645d8162e02c2adc945ca1c6b3
Eugene <nester77722@gmail.com>
2021-05-14 13:21:36 +0300

    Создал класс Logger

---
+++ Project-Module-2-Лекция-3/MyProject/MyProject/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyProject
+{
+    public class Logger
+    {
+        private static Logger _uniqueInstance;
+
+        private DateTime _dateTime;
+
+        private FileService _fileService;
+
+        private Logger()
+        {
+            _fileService = new FileService();
+
+            _dateTime = DateTime.Now;
+            Console.WriteLine(_dateTime.ToLongTimeString());
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_uniqueInstance == null)
+                {
+                    _uniqueInstance = new Logger();
+                }
+
+                return _uniqueInstance;
+            }
+        }
+    }
+}
