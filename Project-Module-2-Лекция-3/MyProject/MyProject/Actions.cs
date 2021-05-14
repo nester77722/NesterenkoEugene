880f8cfb5e591e56ab116de37697c896d4e6944d
Eugene <nester77722@gmail.com>
2021-05-14 15:44:39 +0300

    Создал класс Actions

---
+++ Project-Module-2-Лекция-3/MyProject/MyProject/Actions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public static class Actions
+    {
+        private static Logger _logger = Logger.Instance;
+
+        public static void StartMethod()
+        {
+            _logger.AddLog(LogTypes.Info, "Message frome StartMethod.");
+        }
+
+        public static void Business()
+        {
+            throw new BusinessException("Skipped logic in method");
+        }
+
+        public static void Exception()
+        {
+            throw new Exception("I broke a logic");
+        }
+    }
+}
