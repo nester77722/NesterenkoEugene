24924d03399a1f99711747f940f916158c4c406e
Eugene <nester77722@gmail.com>
2021-05-19 19:55:17 +0300

    Сделал юзеров

---
+++ Project-Module-2-Final/MyProject/MyProject/Users/User.cs
@@ -0,0 +1,17 @@
+﻿using MyProject.Attributes;
+
+namespace MyProject.Users
+{
+    public abstract class User
+    {
+        [RequiredField]
+        public string Login { get; init; }
+
+        [RequiredField]
+        public string Password { get; init; }
+
+        public int Age { get; init; }
+
+        public int Level { get; init; }
+    }
+}
