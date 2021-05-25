8405fb883cd8ca97dd196a41c2942f14c03438c5
Eugene <nester77722@gmail.com>
2021-05-25 17:38:14 +0300

    Сделал Week

---
+++ Project-Module-3-Задание-1/MyProject/MyCollections/Week.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollections
+{
+    public class Week
+    {
+        private string[] _days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        private int _position = -1;
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            while (true)
+            {
+                if (_position < _days.Length - 1)
+                {
+                    _position++;
+                    yield return _days[_position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+        }
+
+        private void Reset()
+        {
+            _position = -1;
+        }
+    }
+}
