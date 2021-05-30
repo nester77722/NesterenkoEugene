61b46bffe827b3c0798ade3ef7c10c45b4218db3
Eugene <nester77722@gmail.com>
2021-05-30 23:50:38 +0300

    Сделал лист ReadOnly

--- Project-Module-3-Задание-2/MyProject/MyCollections/MyLinkedList.cs
+++ Project-Module-3-Задание-2/MyProject/MyCollections/MyLinkedList.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 
 namespace MyCollections
 {
-    public class MyLinkedList<T> : IEnumerable<T>
+    public class MyLinkedList<T> : IEnumerable<T>, IReadOnlyCollection<T>
     {
         private Node<T> _head;
         private Node<T> _tail;
+
+        public MyLinkedList(ICollection<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                AddLast(item);
+            }
+        }
 
         public int Count { get; private set; }
 
@@ -29,108 +37,11 @@ namespace MyCollections
 
                 return head.Value;
             }
-
-            set
-            {
-                if (index > Count || index < 0)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-
-                if (index == Count)
-                {
-                    AddLast(value);
-                }
-                else if (index == 0)
-                {
-                    AddFirst(value);
-                }
-                else
-                {
-                    MyLinkedList<T> newList = CloneList();
-                    Clear();
-
-                    Node<T> head = newList._head;
-                    int i = 0;
-
-                    while (head != null)
-                    {
-                        if (i == index)
-                        {
-                            AddLast(value);
-                        }
-
-                        AddLast(head.Value);
-                        head = head.Next;
-                        i++;
-                    }
-                }
-            }
-        }
-
-        public void AddFirst(T value)
-        {
-            Node<T> node = new Node<T>(value);
-            node.Next = _head;
-            _head = node;
-
-            if (Count == 0)
-            {
-                _tail = _head;
-            }
-
-            Count++;
-        }
-
-        public void AddLast(T value)
-        {
-            Node<T> node = new Node<T>(value);
-
-            if (_head == null)
-            {
-                _head = node;
-                _tail = node;
-            }
-            else
-            {
-                _tail.Next = node;
-                _tail = node;
-            }
-
-            Count++;
-        }
-
-        public void Clear()
-        {
-            Node<T> head = _head;
-
-            while (head != null)
-            {
-                Node<T> node2 = head;
-                head = head.Next;
-                node2.Invalidate();
-            }
-
-            _head = null;
-            Count = 0;
         }
 
         public MyLinkedList<T> CloneList()
         {
-            MyLinkedList<T> newList = new MyLinkedList<T>();
-
-            if (_head == null)
-            {
-                return newList;
-            }
-
-            Node<T> head = _head;
-
-            while (head != null)
-            {
-                newList.AddLast(head.Value);
-                head = head.Next;
-            }
+            MyLinkedList<T> newList = new MyLinkedList<T>(ToArray());
 
             return newList;
         }
@@ -152,55 +63,6 @@ namespace MyCollections
             }
 
             return -1;
-        }
-
-        public bool Remove(T value)
-        {
-            Node<T> node = _head;
-
-            while (node != null)
-            {
-                if (node.Value.Equals(value))
-                {
-                    RemoveNode(node);
-                    return true;
-                }
-
-                node = node.Next;
-            }
-
-            return false;
-        }
-
-        public bool RemoveByIndex(int index)
-        {
-            if (index >= Count || index < 0)
-            {
-                return false;
-            }
-
-            Node<T> node = _head;
-
-            for (int i = 0; i < index; i++)
-            {
-                node = node.Next;
-            }
-
-            RemoveNode(node);
-
-            return true;
-        }
-
-        public bool RemoveEveryEqual(T item)
-        {
-            bool isSuccesfull = false;
-
-            while (Remove(item))
-            {
-                isSuccesfull = true;
-            }
-
-            return isSuccesfull;
         }
 
         public T[] ToArray()
@@ -237,40 +99,36 @@ namespace MyCollections
             return GetEnumerator();
         }
 
-        private void RemoveNode(Node<T> node)
+        private void AddFirst(T value)
         {
-            Node<T> previous = null;
-            Node<T> current = _head;
+            Node<T> node = new Node<T>(value);
+            node.Next = _head;
+            _head = node;
 
-            while (current != null)
+            if (Count == 0)
             {
-                if (current.Equals(node))
-                {
-                    if (previous != null)
-                    {
-                        previous.Next = current.Next;
-
-                        if (current.Next == null)
-                        {
-                            _tail = previous;
-                        }
-                    }
-                    else
-                    {
-                        _head = _head.Next;
-
-                        if (_head == null)
-                        {
-                            _tail = null;
-                        }
-                    }
-
-                    Count--;
-                }
-
-                previous = current;
-                current = current.Next;
+                _tail = _head;
             }
+
+            Count++;
+        }
+
+        private void AddLast(T value)
+        {
+            Node<T> node = new Node<T>(value);
+
+            if (_head == null)
+            {
+                _head = node;
+                _tail = node;
+            }
+            else
+            {
+                _tail.Next = node;
+                _tail = node;
+            }
+
+            Count++;
         }
     }
 }
