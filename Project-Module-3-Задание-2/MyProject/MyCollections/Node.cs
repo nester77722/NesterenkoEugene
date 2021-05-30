371ddab28ffc3a9a6c1e4f87d7cc036005f05682
Eugene <nester77722@gmail.com>
2021-05-30 21:56:05 +0300

    Доделал связной список

---
+++ Project-Module-3-Задание-2/MyProject/MyCollections/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyCollections
+{
+    internal class Node<T>
+    {
+        internal Node(T value)
+        {
+            Value = value;
+        }
+
+        internal T Value { get; set; }
+
+        internal Node<T> Next { get; set; }
+
+        internal void Invalidate()
+        {
+            Next = null;
+            Value = default(T);
+        }
+    }
+}
---
+++ Project-Module-3-Задание-2/MyProject/MyProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using MyCollections;
+
+namespace MyProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyLinkedList<int> list = new MyLinkedList<int>();
+
+            list.AddFirst(4);
+            list.AddFirst(3);
+
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
