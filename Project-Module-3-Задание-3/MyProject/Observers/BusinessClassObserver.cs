5e81c800232bb900248212d426881a27a2ce22b7
Eugene <nester77722@gmail.com>
2021-06-02 21:44:48 +0300

    Первая версия наблюдателя

---
+++ Project-Module-3-Задание-3/MyProject/Observers/BusinessClassObserver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observers
+{
+    public class BusinessClassObserver : IObserver
+    {
+        private Logger.Logger _logger = Logger.Logger.Instance;
+
+        public void Update(string message)
+        {
+            _logger.AddLog(message);
+        }
+    }
+}
