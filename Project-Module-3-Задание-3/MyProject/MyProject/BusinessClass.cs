90d8cc1f7fb3093e52dfd5c863dd57a6f49585ef
Eugene <nester77722@gmail.com>
2021-06-02 21:59:36 +0300

    Создал класс с псевдо логикой и добавил примеров

---
+++ Project-Module-3-Задание-3/MyProject/MyProject/BusinessClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Observers;
+
+namespace MyProject
+{
+    public class BusinessClass : Subject
+    {
+        public void BusinessMethod1()
+        {
+            Notify($"Message from {System.Reflection.MethodBase.GetCurrentMethod().Name}");
+        }
+
+        public void BusinessMethod2()
+        {
+            Notify($"Message from {System.Reflection.MethodBase.GetCurrentMethod().Name}");
+        }
+
+        public void BusinessMethod3()
+        {
+            Notify($"Message from {System.Reflection.MethodBase.GetCurrentMethod().Name}");
+        }
+    }
+}
--- Project-Module-3-Задание-3/MyProject/MyProject/Program.cs
+++ Project-Module-3-Задание-3/MyProject/MyProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Logger;
+using Observers;
 
 namespace MyProject
 {
@@ -6,6 +8,13 @@ namespace MyProject
     {
         public static void Main(string[] args)
         {
+            BusinessClass businessClass = new BusinessClass();
+            LoggerObserver loggerObserver = new LoggerObserver();
+
+            businessClass.Observers += loggerObserver.LogNotyfies;
+            businessClass.BusinessMethod1();
+            businessClass.BusinessMethod2();
+            businessClass.BusinessMethod3();
         }
     }
 }
