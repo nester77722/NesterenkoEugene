a6376c926bf049657ba71031fda81e5fb4db32bd
Eugene <nester77722@gmail.com>
2021-06-21 10:50:49 +0300

    Создал проект под дз

---
+++ Project-Module-3-Задание-7/MyProject/MyProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
