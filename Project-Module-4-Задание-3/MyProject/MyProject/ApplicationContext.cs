d50219b5b5ac5b58b80a311e65021cc454b0250e
Eugene <nester77722@gmail.com>
2021-07-08 14:02:01 +0300

    Сделал конфиг и AppCоntext

---
+++ Project-Module-4-Задание-3/MyProject/MyProject/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyProject
+{
+    public class ApplicationContext : DbContext
+    {
+        private Config _config;
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserAddress> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+        public ApplicationContext(Config config)
+        {
+            _config = config;
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+                
+        }
+    }
+}
