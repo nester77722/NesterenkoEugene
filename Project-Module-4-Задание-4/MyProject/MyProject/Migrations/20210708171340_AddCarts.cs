be05054127071c76ac23737b35829ff84b334952
Eugene <nester77722@gmail.com>
2021-07-08 20:28:21 +0300

    AddCarts Migration

---
+++ Project-Module-4-Задание-4/MyProject/MyProject/Migrations/20210708171340_AddCarts.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProject.Migrations
+{
+    public partial class AddCarts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_Discounts_DiscountId",
+                table: "Cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cart_Users_UserId",
+                table: "Cart");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Cart_CartId1",
+                table: "CartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart");
+
+            migrationBuilder.RenameTable(
+                name: "Cart",
+                newName: "Carts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cart_UserId",
+                table: "Carts",
+                newName: "IX_Carts_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cart_DiscountId",
+                table: "Carts",
+                newName: "IX_Carts_DiscountId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId1",
+                table: "CartItems",
+                column: "CartId1",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Discounts_DiscountId",
+                table: "Carts",
+                column: "DiscountId",
+                principalTable: "Discounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId1",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Discounts_DiscountId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts");
+
+            migrationBuilder.RenameTable(
+                name: "Carts",
+                newName: "Cart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_UserId",
+                table: "Cart",
+                newName: "IX_Cart_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Carts_DiscountId",
+                table: "Cart",
+                newName: "IX_Cart_DiscountId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_Discounts_DiscountId",
+                table: "Cart",
+                column: "DiscountId",
+                principalTable: "Discounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cart_Users_UserId",
+                table: "Cart",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Cart_CartId1",
+                table: "CartItems",
+                column: "CartId1",
+                principalTable: "Cart",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
