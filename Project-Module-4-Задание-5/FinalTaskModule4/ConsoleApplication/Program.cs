b202b8d075936cd59bfc106cf1c76c019fff18ca
Eugene <nester77722@gmail.com>
2021-07-11 08:15:48 +0300

    Добавление студента

--- Project-Module-4-Задание-5/FinalTaskModule4/ConsoleApplication/Program.cs
+++ Project-Module-4-Задание-5/FinalTaskModule4/ConsoleApplication/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using ModulePL;
+using ModuleDal;
+using ModuleDal.Models;
+//using ModulePL;
 
 namespace ConsoleApplication
 {
@@ -7,15 +9,19 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var ctrl = new StudentsController();
+            //var ctrl = new StudentsController();
 
-            var student = ctrl.GetById(1);
+            //var student = ctrl.GetById(1);
 
-            Console.WriteLine($"{student.FullName} {student.Age}");
-            foreach (var item in student.Payments)
-            {
-                Console.WriteLine($"{item.Date} {item.Value}");
-            }
+            //Console.WriteLine($"{student.FullName} {student.Age}");
+            //foreach (var item in student.Payments)
+            //{
+            //    Console.WriteLine($"{item.Date} {item.Value}");
+            //}
+            Student student = new Student() { FirstName = "John", LastName = "Collins", Age = 23 };
+            ModuleContext context = new ModuleContext();
+            context.Add(student);
+            context.SaveChanges();
         }
     }
 }
