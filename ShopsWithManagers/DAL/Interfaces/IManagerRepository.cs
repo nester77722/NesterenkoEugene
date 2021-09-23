6df5624b1d8ef0e2cec068df78717b15f8e74499
Eugene <nester77722@gmail.com>
2021-09-23 17:27:17 +0300

    Created Interfaces for Repositories

---
+++ ShopsWithManagers/DAL/Interfaces/IManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+
+namespace DAL.Interfaces
+{
+    public interface IManagerRepository
+    {
+        void AddNewManager(Manager manager);
+        void UpdateManager(Manager manager);
+        AllManagersResponse GetManagers(int page, int pageSize, ManagerFilters filters);
+        Manager GetManager(int managerId);
+    }
+}
---
+++ ShopsWithManagers/DAL/Interfaces/IWorkingHistoriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+
+namespace DAL.Interfaces
+{
+    public interface IWorkingHistoriesRepository
+    {
+        void AddNewWorkingHistoryLogForManager(int managerId, int shopId);
+        void CompleteWorkingHistoryForManager(int managerId);
+        public IEnumerable<ManagerWorkingHistory> GetManagerWorkingHistory(int managerId);
+    }
+}
