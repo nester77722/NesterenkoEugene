6dc87b1a247d80825b00fd71c7da021494cc5808
Eugene <nester77722@gmail.com>
2021-09-23 18:40:09 +0300

    Added Manager & Shop Services

---
+++ ShopsWithManagers/BLL/Services/ManagerService.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using BLL.Interfaces;
+using BLL.Models;
+using DAL;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Services
+{
+    public class ManagerService : IManagerService
+    {
+        private readonly IWorkingHistoryRepository _workingHistoryRepository;
+        private readonly IManagerRepository _managerRepository;
+        private readonly IMapper _mapper;
+
+        public ManagerService(IManagerRepository managerRepository, IWorkingHistoryRepository  workingHistoryRepository, IMapper mapper)
+        {
+            _managerRepository = managerRepository;
+            _workingHistoryRepository = workingHistoryRepository;
+            _mapper = mapper;
+        }
+
+        public void AddManager(ManagerModel manager)
+        {
+            _managerRepository.AddManager(_mapper.Map<Manager>(manager));
+
+        }
+
+        public void DismissFromWork(int managerId)
+        {
+            var currentManager = _managerRepository.GetManagerById(managerId);
+
+            if (currentManager == null)
+            {
+                throw new Exception("No manager with such id!");
+            }
+
+            if (currentManager.CurrentShopId == null)
+            {
+                throw new Exception("Manager doesn't work!");
+            }
+
+            currentManager.CurrentShopId = null;
+            _managerRepository.UpdateManager(currentManager);
+            _workingHistoryRepository.EndWorkingHistoryForManager(managerId);
+        }
+
+        public void EmployManagerIntoShop(int managerId, int shopId)
+        {
+            var currentManager = _managerRepository.GetManagerById(managerId);
+
+            if (currentManager == null)
+            {
+                throw new Exception("No manager with such id!");
+            }
+
+            if (currentManager.CurrentShopId.HasValue)
+            {
+                throw new Exception("Manager is already working!");
+            }
+
+            currentManager.CurrentShopId = shopId;
+            _managerRepository.UpdateManager(currentManager);
+            _workingHistoryRepository.AddWorkingHistoryLogForManager(managerId, shopId);
+        }
+
+        public ManagerModel GetManager(int managerId)
+        {
+            var manager = _managerRepository.GetManagerById(managerId);
+            if(manager is null)
+            {
+                throw new Exception("No manager with such id!");
+            }
+
+            var managerModel = _mapper.Map<ManagerModel>(manager);
+
+            return managerModel;
+        }
+
+        public AllManagersResponseModel GetManagers(int page, int pageSize, ManagerFilters filters)
+        {
+            var allManagers = _managerRepository.GetManagers(page, pageSize, filters);
+
+            return _mapper.Map<AllManagersResponseModel>(allManagers);
+        }
+    }
+}
